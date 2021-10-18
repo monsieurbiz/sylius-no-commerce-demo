3ef7dc4f6f65b57120798fc4ec34c06da36a6b58
Maxime Huran <m.huran@monsieurbiz.com>
2021-10-18 23:02:42 +0200

    Add Author books views

--- apps/sylius/src/Repository/BookRepository.php
+++ apps/sylius/src/Repository/BookRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Pagerfanta;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
 class BookRepository extends EntityRepository implements BookRepositoryInterface
@@ -16,5 +17,16 @@ class BookRepository extends EntityRepository implements BookRepositoryInterface
             ->leftJoin('o.translations', 'translation', 'WITH', 'translation.locale = :localeCode')
             ->setParameter('localeCode', $localeCode)
         ;
+    }
+
+    public function createAuthorListQueryBuilder(string $localeCode, string $authorId): Pagerfanta
+    {
+        $queryBuilder = $this
+            ->createListQueryBuilder($localeCode)
+            ->where('IDENTITY(o.author) = :authorId')
+            ->setParameter('authorId', (int) $authorId)
+            ->orderBy('translation.title')
+        ;
+        return EntityRepository::getPaginator($queryBuilder);
     }
 }
--- apps/sylius/src/Repository/BookRepositoryInterface.php
+++ apps/sylius/src/Repository/BookRepositoryInterface.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Pagerfanta;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 interface BookRepositoryInterface extends RepositoryInterface
 {
     public function createListQueryBuilder(string $localeCode): QueryBuilder;
+
+    public function createAuthorListQueryBuilder(string $localeCode, string $authorId): Pagerfanta;
 }
